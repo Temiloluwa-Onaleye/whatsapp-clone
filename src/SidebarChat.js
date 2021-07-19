23db89f31e3d65235b17ba193610f43025c58fcf
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2021-07-19 17:20:04 +0100

    whatsapp-clone

---
+++ src/SidebarChat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SidebarChat.css';
+function SidebarChat() {
+    return (
+        <div className="sidebarchat">
+            <h1>SidebarChat</h1>
+        </div>
+    )
+}
+
+export default SidebarChat
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCKj753i5t92pQye3-_u-qGpnm4Ltbzs7k",
+    authDomain: "whatsapp-clone-49e45.firebaseapp.com",
+    projectId: "whatsapp-clone-49e45",
+    storageBucket: "whatsapp-clone-49e45.appspot.com",
+    messagingSenderId: "232319889009",
+    appId: "1:232319889009:web:5f585f2b14b8e356d7f5d0",
+    measurementId: "G-03ZVNYB82Q"
+  };
