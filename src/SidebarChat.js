2c791d0dba700238e6a4ef740765b17fbc931e1c
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2021-07-21 15:02:52 +0100

    whatsapp clone

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import db from './firebase';
 import './SidebarChat.css';
-function SidebarChat({addNewChat}) {
+function SidebarChat({addNewChat, name, id}) {
     const [seed, setSeed ] = useState('');
 
     useEffect(() => {
@@ -13,16 +15,23 @@ const createChat = () => {
 const roomName = prompt('Please enter name for chat');
 if (roomName) {
     //do some clever database stuff here
+    db.collection( 'rooms').add({
+        name: roomName,
+
+    })
 }
 };
     return !addNewChat ? (
+           //Create dynamic url to room with that id.
+    <Link to={`/rooms/${id}`}>
         <div className="sidebarChat">
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className="sidebarChat__info">
-            <h2>Room name</h2>
+            <h2>{name}</h2>
             <p>last message ...</p>
             </div>
         </div>
+        </Link>
     ) : (
         <div onClick={createChat} className="sidebarChat" >
         <h2>Add new chat</h2>
